ceff04f470b24c9273d65f9f5454767eb0ab85cc
salted-fish-cky <1324775254@qq.com>
2019-03-10 13:53:23 +0800

    springboot-mybatis逆向工程

---
+++ springboot-mybatis-generator/src/main/java/com/cky/mapper/ChatMsgMapper.java
@@ -0,0 +1,7 @@
+package com.cky.mapper;
+
+import com.cky.pojo.ChatMsg;
+import com.cky.utils.MyMapper;
+
+public interface ChatMsgMapper extends MyMapper<ChatMsg> {
+}
---
+++ springboot-mybatis-generator/src/main/java/com/cky/mapper/FriendsRequestMapper.java
@@ -0,0 +1,7 @@
+package com.cky.mapper;
+
+import com.cky.pojo.FriendsRequest;
+import com.cky.utils.MyMapper;
+
+public interface FriendsRequestMapper extends MyMapper<FriendsRequest> {
+}
